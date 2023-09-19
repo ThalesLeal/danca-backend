9dc6da83813fc89626384994c55e49acc6fc9b8d
Livio Ribeiro <livio@codata.pb.gov.br>
2023-09-19 17:36:44 +0000

    Update settings.py

--- _conf/settings.py
+++ _conf/settings.py
@@ -47,6 +47,8 @@ SECRET_KEY = (
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
+
 # Application definition
 
 INSTALLED_APPS = [
