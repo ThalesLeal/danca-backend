5db57177db9c920c701899d5af390a5bf999c1c0
Murilo Doria <murilodoria@codata.pb.gov.br>
2025-02-26 09:46:20 -0300

    Transferido o css e o javascript do sidebar para arquivos próprios

---
+++ _core/static/js/scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.querySelector('.main-content');
+    const sidebarToggle = document.getElementById('sidebarToggle');
+
+    sidebarToggle.addEventListener('click', function() {
+        sidebar.classList.toggle('collapsed');
+        mainContent.classList.toggle('expanded');
+    });
+});
