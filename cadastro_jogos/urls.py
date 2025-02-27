7790f221775755bb61c5286159c5306fb3b5ed13
pedro.santos <pedro.santos@codata.pb.gov.br>
2025-02-27 11:33:25 -0300

    alterado nome da rota de criação de usuário

--- cadastro_jogos/urls.py
+++ cadastro_jogos/urls.py
@@ -3,7 +3,7 @@ from .views import usuario_list, create_usuario, read_usuario, update_usuario, d
 
 urlpatterns = [
     path('', usuario_list, name='usuario_list'),
-    path('create/', create_usuario, name='create_usuario'),
+    path('usuarios/create/', create_usuario, name='create_usuario'),
     path('usuarios/<uuid:id>/', read_usuario, name='read_usuario'),
     path('usuarios/<uuid:id>/update/', update_usuario, name='update_usuario'),
     path('usuarios/<uuid:id>/delete/', delete_usuario, name='delete_usuario'),
