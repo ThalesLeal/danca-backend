57f8305f9dfbbb57b8b1b814d8038fc38bc73536
pedro.santos <pedro.santos@codata.pb.gov.br>
2025-03-12 12:55:46 -0300

    adicionada CBV para UsuarioJogos, com a função get.

--- cadastro_jogos/views.py
+++ cadastro_jogos/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import UsuarioJogos
 from django.contrib import messages
@@ -9,13 +7,47 @@ from .forms import UsuarioJogosForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from django.core.paginator import Paginator
+from django.views import View
+from django.utils.decorators import method_decorator
 from .utils import PERFIL_CHOICES
 import re
-# Create a new user
+
+@method_decorator(login_required, name='dispatch')
+class UsuarioJogosView(View):
+    form_class = UsuarioJogosForm()
+    template_list = "usuario_jogos/list.html"
+    template_form = "usuario_jogos/form.html"
+
+    def get(self, request):
+        query = request.GET.get('q')
+        if query:
+            usuarios = UsuarioJogos.objects.filter(nome__icontains=query).order_by('nome')
+        else:
+            usuarios = UsuarioJogos.objects.all().order_by('nome')
+
+        paginator = Paginator(usuarios, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, self.template_list, {'page_obj': page_obj})
+    
+    # def post(self, request):
+    #     data = request.POST.copy()
+    #     cpf = re.sub(r'\D', '', request.POST.get('cpf'))  # Remove caracteres não numéricos do CPF
+    #     telefone = re.sub(r'\D', '', request.POST.get('telefone'))  # Remove caracteres não numéricos do telefone
+
+    #     data["cpf"] = cpf
+    #     data["telefone"] = telefone
+
+    #     form = self.form_class(data)
+
+    #     if form.is_valid():
+    #         form.save()
+    #         messages.success(request, 'Usuário criado com sucesso')
+    #         return redirect('/')
 
 
 @login_required
-@never_cache
 def create_usuario(request):
     if request.method == 'POST':
         data = request.POST.copy()
@@ -84,23 +116,3 @@ def delete_usuario(request, id):
         messages.success(request, 'Usuário deletado com sucesso!')
         return redirect('usuario_list')  # Redirect to user list after deletion
     return render(request, 'delete_usuario.html', {'usuario': usuario})
-
-
-# List all users
-@login_required
-@never_cache
-def usuario_list(request):
-    query = request.GET.get('q')  # Obtém o parâmetro de busca da URL
-    if query:
-        usuarios = UsuarioJogos.objects.filter(nome__icontains=query).order_by('nome')  # Filtra usuários pelo nome
-    else:
-        usuarios = UsuarioJogos.objects.all().order_by('nome')  # Retorna todos os usuários se não houver busca
-
-    # Paginação
-    paginator = Paginator(usuarios, 10)  # Mostra 10 usuários por página
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    total_usuarios = usuarios.count()  # Total de registros
-
-    return render(request, 'index.html', {'page_obj': page_obj, 'total_usuarios': total_usuarios})
