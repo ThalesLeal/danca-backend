d62dde05c3ca32ec81d84f696bf95220cc3553dd
pedro.santos <pedro.santos@codata.pb.gov.br>
2025-03-12 13:15:19 -0300

    cria view de listagem de usuarios

--- cadastro_jogos/urls.py
+++ cadastro_jogos/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import create_usuario, read_usuario, update_usuario, delete_usuario, UsuarioJogosView
+from .views import create_usuario, read_usuario, update_usuario, delete_usuario, UsuarioJogosListView
 
 urlpatterns = [
-    path('', UsuarioJogosView.as_view(), name='list_usuarios'),
+    path('usuarios/', UsuarioJogosListView.as_view(), name='list_usuarios'),
     path('usuarios/create/', create_usuario, name='create_usuario'),
     path('usuarios/<uuid:id>/', read_usuario, name='read_usuario'),
     path('usuarios/<uuid:id>/update/', update_usuario, name='update_usuario'),
--- cadastro_jogos/views.py
+++ cadastro_jogos/views.py
@@ -1,5 +1,3 @@
-from django.views.generic.edit import UpdateView
-from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import UsuarioJogos
 from django.contrib import messages
@@ -8,15 +6,34 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from django.core.paginator import Paginator
 from django.views import View
+from django.views.generic.list import ListView
 from django.utils.decorators import method_decorator
 from .utils import PERFIL_CHOICES
 import re
 
 @method_decorator(login_required, name='dispatch')
-class UsuarioJogosView(View):
+class UsuarioJogosListView(ListView):
+    model = UsuarioJogos
+    paginate_by = 10
+    template_name = "usuario_jogos/list.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return UsuarioJogos.objects.filter(nome__icontains=query).order_by('nome')
+        else:
+            return UsuarioJogos.objects.all().order_by('nome')
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q', '')
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class UsuarioJogosFormView(View):
     form_class = UsuarioJogosForm()
-    template_list = "usuario_jogos/list.html"
-    template_form = "usuario_jogos/form.html"
+    template_name = "usuario_jogos/form.html"
 
     def get(self, request):
         query = request.GET.get('q')
@@ -31,20 +48,20 @@ class UsuarioJogosView(View):
 
         return render(request, self.template_list, {'page_obj': page_obj})
     
-    # def post(self, request):
-    #     data = request.POST.copy()
-    #     cpf = re.sub(r'\D', '', request.POST.get('cpf'))  # Remove caracteres não numéricos do CPF
-    #     telefone = re.sub(r'\D', '', request.POST.get('telefone'))  # Remove caracteres não numéricos do telefone
+    def post(self, request):
+        data = request.POST.copy()
+        cpf = re.sub(r'\D', '', request.POST.get('cpf'))  # Remove caracteres não numéricos do CPF
+        telefone = re.sub(r'\D', '', request.POST.get('telefone'))  # Remove caracteres não numéricos do telefone
 
-    #     data["cpf"] = cpf
-    #     data["telefone"] = telefone
+        data["cpf"] = cpf
+        data["telefone"] = telefone
 
-    #     form = self.form_class(data)
+        form = self.form_class(data)
 
-    #     if form.is_valid():
-    #         form.save()
-    #         messages.success(request, 'Usuário criado com sucesso')
-    #         return redirect('/')
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário criado com sucesso')
+            return redirect('/')
 
 
 @login_required
